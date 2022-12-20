beea096d442600541fe219c7aa605efcf0b54a82
Ekaterina Rykova <katrykova2002@yandex.ru>
2022-12-20 21:37:17 +0300

    debug false

--- api_mob/settings.py
+++ api_mob/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-az%5eb!mhn8*3dvm5@w#^8#z^=zho*ocg)ty3bydfuki!4jo7^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
